854b0cc5f44a24749de6d2d1754b411230382941
Romain Lavoix <romain.lavoix@gmail.com>
2018-03-23 17:37:50 +1100

    googlebot render correctly when removing decorators

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,10 @@
 import Layout from "../components/layout";
-import { initStore } from "../store";
-import { Provider } from "mobx-react";
 
 class Welcome extends React.Component {
-  static async getInitialProps(ctx) {
-    const isServer = !!ctx.req;
-    const store = initStore(isServer);
-    return { lastUpdate: store.lastUpdate, isServer };
-  }
 
-  constructor(props) {
-    super(props);
-    this.store = initStore(props.isServer, props.lastUpdate);
-  }
 
   render() {
-    return (
-      <Provider appStore={this.store}>
-        <Layout title={'gougle.nc'}>
-        </Layout>
-      </Provider>
-    );
+    return <Layout title={"gougle.nc"} />;
   }
 }
 
