71b6b0ab2e508b6e0b8d240f9e1c657bc16f4da4
Romain Lavoix <romain.lavoix@gmail.com>
2018-03-24 14:31:40 +1100

    async fonts load - service workers integration

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
 import Layout from "../components/layout";
+import Fonts from "../utils/fonts";
 
 class Welcome extends React.Component {
-
+  componentDidMount() {
+    Fonts();
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then(registration => {
+          console.log("service worker registration successful");
+        })
+        .catch(err => {
+          console.warn("service worker registration failed", err.message);
+        });
+    }
+  }
 
   render() {
     return <Layout title={"gougle.nc"} />;
