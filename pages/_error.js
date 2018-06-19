ca83a1c764065de77cb87f1a64c1ddccff4503d3
Romain Lavoix <romain.lavoix@gmail.com>
2018-06-19 16:03:24 +1100

    better 404

--- pages/_error.js
+++ pages/_error.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 export default class Error extends React.Component {
   static getInitialProps({ res, err }) {
@@ -8,85 +9,103 @@ export default class Error extends React.Component {
 
   render() {
     return (
-      <div style={{
-        height: 'calc(100vh - 100px)'
-      }}>
+      <div
+        style={{
+          height: "calc(100vh - 50px)"
+        }}
+      >
         <table
           id="wrapper"
           style={{
             width: "100%",
-            height: "100%",
+            height: "70%",
             margin: 0,
             padding: 0,
             border: 0
           }}
         >
-          <tr>
-            <td
-              style={{
-                veticalAlign: "middle",
-                textAlign: "center"
-              }}
-            >
-              <picture>
-                <source
-                  media={`(min-width: 960px)`}
-                  srcSet={`
+          <tbody>
+            <tr>
+              <td
+                style={{
+                  verticalAlign: "middle",
+                  textAlign: "center"
+                }}
+              >
+                <picture>
+                  <source
+                    media={`(min-width: 960px)`}
+                    srcSet={`
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_700,dpr_1.0/qwarx-404.png,
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_700,dpr_2.0/qwarx-404.png 2x,
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_700,dpr_3.0/qwarx-404.png 3x
           `}
-                />
-                <source
-                  media={`(max-width: 959px)`}
-                  srcSet={`
+                  />
+                  <source
+                    media={`(max-width: 959px)`}
+                    srcSet={`
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_300,dpr_1.0/qwarx-404.png,
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_300,dpr_2.0/qwarx-404.png 2x,
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_300,dpr_3.0/qwarx-404.png 3x
           `}
-                />
-                <img
-                  src={`
+                  />
+                  <img
+                    src={`
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_700,dpr_1.0/qwarx-404.png`}
-                  srcSet={`
+                    srcSet={`
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_700,dpr_1.0/qwarx-404.png,
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_700,dpr_2.0/qwarx-404.png 2x,
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_700,dpr_3.0/qwarx-404.png 3x
           `}
-                  alt={`erreur 404`}
-                />
-              </picture>
-              
-              <picture>
-                <source
-                  media={`(min-width: 960px)`}
-                  srcSet={`
+                    alt={`erreur 404`}
+                  />
+                </picture>
+              </td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td
+                style={{
+                  verticalAlign: "middle",
+                  textAlign: "center"
+                }}
+              >
+                <Link prefetch href="/">
+                  <a>
+                    <picture>
+                      <source
+                        media={`(min-width: 960px)`}
+                        srcSet={`
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_600,dpr_1.0/qwarx-back.png,
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_600,dpr_2.0/qwarx-back.png 2x,
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_600,dpr_3.0/qwarx-back.png 3x
           `}
-                />
-                <source
-                  media={`(max-width: 959px)`}
-                  srcSet={`
+                      />
+                      <source
+                        media={`(max-width: 959px)`}
+                        srcSet={`
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_250,dpr_1.0/qwarx-back.png,
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_250,dpr_2.0/qwarx-back.png 2x,
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_250,dpr_3.0/qwarx-back.png 3x
           `}
-                />
-                <img
-                  src={`
+                      />
+                      <img
+                        src={`
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_600,dpr_1.0/qwarx-back.png`}
-                  srcSet={`
+                        srcSet={`
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_600,dpr_1.0/qwarx-back.png,
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_600,dpr_2.0/qwarx-back.png 2x,
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_600,dpr_3.0/qwarx-back.png 3x
           `}
-                  alt={`erreur 404`}
-                />
-              </picture>
-            </td>
-          </tr>
+                        alt={`erreur 404`}
+                      />
+                    </picture>
+                  </a>
+                </Link>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     );
