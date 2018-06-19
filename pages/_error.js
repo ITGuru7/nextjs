8490705a284dee22d91fc7c33adab46013170892
Romain Lavoix <romain.lavoix@gmail.com>
2018-06-19 15:41:52 +1100

    better 404

--- pages/_error.js
+++ pages/_error.js
@@ -8,41 +8,87 @@ export default class Error extends React.Component {
 
   render() {
     return (
-      <picture
-        style={{
-          display: "block",
-          marginLeft: "auto",
-          marginRight: "auto",
-          width: "50%"
-        }}
-      >
-        <source
-          media={`(min-width: 960px)`}
-          srcSet={`
+      <div style={{
+        height: 'calc(100vh - 100px)'
+      }}>
+        <table
+          id="wrapper"
+          style={{
+            width: "100%",
+            height: "100%",
+            margin: 0,
+            padding: 0,
+            border: 0
+          }}
+        >
+          <tr>
+            <td
+              style={{
+                veticalAlign: "middle",
+                textAlign: "center"
+              }}
+            >
+              <picture>
+                <source
+                  media={`(min-width: 960px)`}
+                  srcSet={`
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_700,dpr_1.0/qwarx-404.png,
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_700,dpr_2.0/qwarx-404.png 2x,
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_700,dpr_3.0/qwarx-404.png 3x
           `}
-        />
-        <source
-          media={`(max-width: 959px)`}
-          srcSet={`
+                />
+                <source
+                  media={`(max-width: 959px)`}
+                  srcSet={`
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_300,dpr_1.0/qwarx-404.png,
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_300,dpr_2.0/qwarx-404.png 2x,
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_300,dpr_3.0/qwarx-404.png 3x
           `}
-        />
-        <img
-          src={`
+                />
+                <img
+                  src={`
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_700,dpr_1.0/qwarx-404.png`}
-          srcSet={`
+                  srcSet={`
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_700,dpr_1.0/qwarx-404.png,
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_700,dpr_2.0/qwarx-404.png 2x,
           https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_700,dpr_3.0/qwarx-404.png 3x
           `}
-          alt={`erreur 404`}
-        />
-      </picture>
+                  alt={`erreur 404`}
+                />
+              </picture>
+              
+              <picture>
+                <source
+                  media={`(min-width: 960px)`}
+                  srcSet={`
+          https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_600,dpr_1.0/qwarx-back.png,
+          https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_600,dpr_2.0/qwarx-back.png 2x,
+          https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_600,dpr_3.0/qwarx-back.png 3x
+          `}
+                />
+                <source
+                  media={`(max-width: 959px)`}
+                  srcSet={`
+          https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_250,dpr_1.0/qwarx-back.png,
+          https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_250,dpr_2.0/qwarx-back.png 2x,
+          https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_250,dpr_3.0/qwarx-back.png 3x
+          `}
+                />
+                <img
+                  src={`
+          https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_600,dpr_1.0/qwarx-back.png`}
+                  srcSet={`
+          https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_600,dpr_1.0/qwarx-back.png,
+          https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_600,dpr_2.0/qwarx-back.png 2x,
+          https://res.cloudinary.com/clactacom/image/upload/f_auto,q_auto,c_scale,w_600,dpr_3.0/qwarx-back.png 3x
+          `}
+                  alt={`erreur 404`}
+                />
+              </picture>
+            </td>
+          </tr>
+        </table>
+      </div>
     );
   }
 }
